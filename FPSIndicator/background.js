680e328adb6190d49a2977675eee497078e900d7
Arty <33781032+ItzArty@users.noreply.github.com>
2022-11-19 23:58:05 +0100

    Uploaded background.js

---
+++ FPSIndicator/background.js
@@ -0,0 +1,13 @@
+chrome.action.onClicked.addListener( ( tab ) => {
+
+	chrome.tabs.query( { active: true, currentWindow: true }, ( tabs ) => {
+
+		chrome.tabs.sendMessage( tabs[ 0 ].id, {
+
+			message: "toggle"
+
+		} );
+
+	} );
+
+} );
